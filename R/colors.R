b2d12caf482e15afbdf1e75c1999c66316d00589
EdwinTh <edwinthoen@hotmail.com>
2017-12-13 11:56:36 +0100

    saved cols

---
+++ R/colors.R
@@ -0,0 +1,40 @@
+milkmaid <-
+  c("red(skirt)"        = "#4F1B10",
+    "blue(table cloth)" = "#376597",
+    "green(sleeve)"     = "#556246",
+    "yellow(buste)"     = "#CCAF69",
+    "white(cap)"        = "#ECE5D3",
+    "blue(skirt)"       = "#104A8A",
+    "brown(basket)"     = "#C5AC8E",
+    "green(table cloth" = "#889F95",
+    "organge(floor)"    = "#6B3D26",
+    "blue(vase)"        = "#00295D")
+
+pearl_earring <-
+  c("red(lips)"    = "#7C3A2C",
+    "skin"         = "#EDD8C5",
+    "blue(scarf1)" = "#7F8DBE",
+    "blue(scarf2)" = "#394165",
+    "white(colar)" = "#FCF9F0",
+    "gold(dress)"  = "#B1934A",
+    "black(background)" = "#100F14",
+    "grey(scarf3)"      = "#646C7F",
+    "yellow(scarf4)"    = "#EEDA9D")
+
+staalmeesters <-
+  c("red(tablecloth)" = "#701B06",
+    "brown(panel)"    = "#503C21",
+    "white(colar)"    = "#D7E1D6",
+    "black(cloak)"    = "#060A0D",
+    "skin"            = "#C1A86F")
+
+view_on_Delft <-
+  c("blue(sky)" = "#A7BEE8",
+    "yellow(tower)" = "#E0BB63",
+    "white(cloud1)" = "#FDF9F8",
+    "brown(cloud2)" = "#938F60",
+    "red(roofs)"    = "#A04437",
+    "yellow(quay)"  = "#EFBF6A",
+    "brown(docks)"  = "#8E7C56",
+    "blue(sky2)"    = "#CDD4E4",
+    "brown(gate)"   = "#8B6C4F")
